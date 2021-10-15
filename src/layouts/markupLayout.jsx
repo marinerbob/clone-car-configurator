8f595dc5d7b4ceb4cf887c082b90149b3638e16a
marinerbob <robobson@mail.ru>
2021-10-15 14:25:26 +0300

    renamed footer

--- src/layouts/markupLayout.jsx
+++ src/layouts/markupLayout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { Container } from "react-bootstrap";
 import Menu from '../components/menu';
-import Footer from '../components/footer';
+import Footer from '../components/priceFooter';
 
 import "./markupLayout.css";
 
